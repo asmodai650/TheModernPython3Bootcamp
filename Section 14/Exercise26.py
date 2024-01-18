ebe03defc4cb43384b84be18eabb2b65858f2edc
asmodai650@protonmail.com <asmodai650@protonmail.com>
2024-01-18 16:55:30 -0600

    completed 26 and 27

--- Section 14/Exercise26.py
+++ Section 14/Exercise26.py
@@ -1,1 +1,7 @@
 
+#Create a dictionary called user_info
+#and add at least 3 key value pairs to it
+#(totally up to you what they are)
+
+user_info = {'a': '1', 'b': '2', 'c': '3'}
+print(user_info)
