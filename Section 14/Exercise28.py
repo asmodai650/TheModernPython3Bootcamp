87c6049e4855f2bae8c6c38ffc6d9e16ca63474f
asmodai650@protonmail.com <asmodai650@protonmail.com>
2024-01-18 17:00:50 -0600

    completed 28

--- Section 14/Exercise28.py
+++ Section 14/Exercise28.py
@@ -1,7 +1,16 @@
 ##Given the provided dictionary of donations:
 
-donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0) 
+#donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0) 
 
 #Use a loop to calculate the total value of the donations.
 #Save the result to a variable called total_donations
 
+# DON'T TOUCH PLEASE!
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
+# DON'T TOUCH PLEASE!
+
+total_donations = 0
+for val in donations.values():
+   total_donations += val
+   
+print(total_donations)
